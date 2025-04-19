032765487b836d560877829c2b4c7b4e4121a554
ohdyo <smart5572@naver.com>
2025-04-19 18:13:46 +0900

    update settings.y allowed_hosts

--- travel_checker/travel_checker/settings.py
+++ travel_checker/travel_checker/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost","43.200.181.58"]
+# ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
